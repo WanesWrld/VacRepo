587a50128a0f31a2aefa73974d9cab7c5c3a4868
WanesWrld <tendo10tc@gmail.com>
2021-08-18 10:01:14 +0200

    clean code
    code with no updates and alterations

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VaccineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Vaccine Registration!");
+
+            Console.WriteLine("");
+            Console.WriteLine("Name: ");
+            String Name = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Surname: ");
+            string Surname = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Gender: ");
+            string Gender = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Date of Birth: ");
+            string DOB = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("ID: ");
+            var Idnum = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Province: ");
+            string House_number = Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Congradulations " + Name +" "+ Surname + " you have successfully registered for your vaccine");
+        }
+    }
+}
