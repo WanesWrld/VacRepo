84f34889de6eae72c39c6548b517799baf21cb76
WanesWrld <89078148+WanesWrld@users.noreply.github.com>
2021-08-18 11:48:07 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,6 +33,7 @@ namespace VaccineApp
             string  money = Console.ReadLine();
             Console.Clear();
             // output
+            //thabang
             Console.WriteLine("Congradulations " + Name +" "+ Surname + " you have successfully registered for your vaccine");
         }
     }
